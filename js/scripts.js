b930cf1e53782141bb6ee2340d88f88352e18d46
ksxmore <78694057+ksxmore@users.noreply.github.com>
2021-02-07 20:38:47 +0100

    Mniejsze usprawnienia

--- js/scripts.js
+++ js/scripts.js
@@ -23,29 +23,29 @@ function clearTab() {
 
 function SetKili(wynik) {
 
-   if (wynik >= 0.1 && wynik < 0.25) {
+   if (wynik >= 0.1 && wynik <= 0.25) {
       document.getElementById("k0").classList.add("border-2", "border-dark", "rounded");
-   } else if (wynik >= 0.25 && wynik < 0.5) {
+   } else if (wynik > 0.25 && wynik <= 0.5) {
       document.getElementById("k1").classList.add("border-2", "border-dark", "rounded");
-   } else if (wynik >= 0.5 && wynik < 0.8) {
+   } else if (wynik > 0.5 && wynik <= 0.8) {
       document.getElementById("k2").classList.add("border-2", "border-dark", "rounded");
-   } else if (wynik >= 0.8 && wynik < 1.2) {
+   } else if (wynik > 0.8 && wynik <= 1.2) {
       document.getElementById("k3").classList.add("border-2", "border-dark", "rounded");
-   } else if (wynik >= 1.2 && wynik < 1.7) {
+   } else if (wynik > 1.2 && wynik <= 1.7) {
       document.getElementById("k4").classList.add("border-2", "border-dark", "rounded");
-   } else if (wynik >= 1.7) {
+   } else if (wynik > 1.7) {
       document.getElementById("k5").classList.add("border-2", "border-dark", "rounded");
    }
 }
 
 
 function Calculator() {
-   var a = Number(document.inputy.piwo.value);
-   var b = Number(document.inputy.woda.value);
-   var c = Number(document.inputy.wino.value);
+   var a = Number(document.getElementById('piwo').value);
+   var b = Number(document.getElementById('woda').value);
+   var c = Number(document.getElementById('wino').value);
    var wynik = 0;
 
-   if (document.inputy.checkbox.checked) {
+   if (document.getElementById('pite').checked) {
       wynik = ((a + (b * 0.018) + (c * 0.005)) * 1.1) / 10;
    } else {
       wynik = (a + (b * 0.018) + (c * 0.005)) / 10;
@@ -58,4 +58,6 @@ function Calculator() {
 
 button.addEventListener('click', clearTab, false);
 button.addEventListener('click', Calculator, false);
-
+button.addEventListener("click", function (event) {
+   event.preventDefault();
+}, false);
